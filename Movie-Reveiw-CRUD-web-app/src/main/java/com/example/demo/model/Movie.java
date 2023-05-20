c4f5732e71e532073aa5e9ac92bce611f6788390
DJsynther42 <128554237+DJsynther42@users.noreply.github.com>
2023-05-20 18:11:48 -0500

    Add files via upload

---
+++ Movie-Reveiw-CRUD-web-app/src/main/java/com/example/demo/model/Movie.java
@@ -0,0 +1,62 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Movies")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "director")
+	private String director;
+	
+	@Column(name = "year")
+	private String year;
+	
+	@Column(name = "rating")
+	private String rating;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+}
+
