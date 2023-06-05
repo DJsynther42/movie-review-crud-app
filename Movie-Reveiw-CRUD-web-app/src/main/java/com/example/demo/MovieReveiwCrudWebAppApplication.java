33c94b2f35c41c3e56df2e95b6c793842d65dffc
DJsynther42 <128554237+DJsynther42@users.noreply.github.com>
2023-06-04 21:07:55 -0500

    Update MovieReveiwCrudWebAppApplication.java

--- Movie-Reveiw-CRUD-web-app/src/main/java/com/example/demo/MovieReveiwCrudWebAppApplication.java
+++ Movie-Reveiw-CRUD-web-app/src/main/java/com/example/demo/MovieReveiwCrudWebAppApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieReveiwCrudWebAppApplication {
-
+	
+	/**
+	 * @author David Burleson
+	 * main is the main method
+	 * @param args String array 
+	 */	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReveiwCrudWebAppApplication.class, args);
 	}
